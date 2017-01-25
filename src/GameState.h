38dde273eda1dc3fb455f9c9462cf6dfce29d641
DESKTOP-BB9KND1\ghoofman <ghoofman@ostusa.com>
2017-01-24 20:11:44 -0500

    basic scripting

--- src/GameState.h
+++ src/GameState.h
@@ -3,10 +3,16 @@
 #include "./OPengine.h"
 
 class ExampleState : public OPgameState {
+
+	OPjavaScriptV8Compiled SCRIPT;
+
     void Init(OPgameState* last);
     OPint Update(OPtimer* time);
     void Render(OPfloat delta);
     OPint Exit(OPgameState* next);
+public:
+	void Wrap(Handle<Object> exports);
+	static void Wrapper(Handle<Object> exports);
 };
 
 extern ExampleState GS_EXAMPLE;
